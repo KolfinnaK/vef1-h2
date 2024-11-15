6f591a4a30f1a749003426aea501f0b27f0436c5
KolfinnaK <kolfinnakolbeinsdottir@gmail.com>
2024-11-15 12:13:51 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module", // module - ekki commonjs
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
